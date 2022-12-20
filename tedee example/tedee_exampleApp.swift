7c1b70353870269ba186973d6fe0a50b4ab6720b
Mateusz Samosij <m.samosij@gmail.com>
2022-12-20 10:39:40 +0100

    Initial Commit

---
+++ tedee example/tedee_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tedee_exampleApp.swift
+//  tedee example
+//
+//  Created by Mateusz Samosij on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct tedee_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
